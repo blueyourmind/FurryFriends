8d28683830b7713ba7d542483bc13c5ba3b8c15e
Thierry Maclou <raymondmclou@gmail.com>
2023-10-26 20:09:28 +0400

    cloudinary

--- db/migrate/20231023154042_create_pets.rb
+++ db/migrate/20231023154042_create_pets.rb
@@ -3,12 +3,11 @@ class CreatePets < ActiveRecord::Migration[7.1]
     create_table :pets do |t|
       t.string :name
       t.string :breed
-      t.string :location
       t.integer :age
       t.text :story
       t.string :species
       t.date :found_when
-
+      t.string :status
       t.timestamps
     end
   end
